714db9224d062a0909cdc5611b564bf91d6b12e1
Vivek kumar <vivekkumarcs23@bsacet.org>
2026-06-11 12:49:01 +0530

    Time: 0 ms (100%), Space: 71.8 MB (14.52%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      long long maxi= LLONG_MIN;
+      long long sum =0;
+      for(int i=0; i<nums.size(); i++){
+        sum += nums[i];
+        if(sum > maxi){
+            maxi = sum;
+        }
+        if(sum <0){
+            sum=0;
+        }
+      }  
+      return maxi;
+    }
+};
